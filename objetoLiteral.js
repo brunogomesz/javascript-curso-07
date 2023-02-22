d53e67cb0f8ae2743ba06700007faba8d5d318cd
brunogomesz <62654168+brunogomesz@users.noreply.github.com>
2023-02-22 18:51:04 -0300

    fim do curso

---
+++ objetoLiteral.js
@@ -0,0 +1,32 @@
+const user = {
+    nome: "Juliana",
+    email: "j@j.com",
+    nascimento: "2021/01/01",
+    role: "estudante",
+    ativo: true,
+    exibirInfos: function() {
+      console.log(this.nome, this.email)
+    }
+  }
+  
+  const admin = {
+    nome: "Mariana",
+    email: "m@m.com",
+    role: "admin",
+    criarCurso() {
+      console.log('curso criado!')
+    }
+  }
+  
+  
+  Object.setPrototypeOf(admin, user) // aqui o admin está herdando métodos do user
+  admin.criarCurso()
+  admin.exibirInfos()
+
+  
+  
+  
+  
+  
+  
+  
